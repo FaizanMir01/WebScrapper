085b87fc67286bf5cfadeb402acf56f38b3bed8e
Faizan Manzoor Mir <141254735+FaizanMir01@users.noreply.github.com>
2024-01-12 12:07:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def extract_urls(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.text,'html.parser')
+
+        # Find all anchor tags (a) with an href attribute
+        links = soup.find_all('a', href=True)
+
+        # Extract and print the URLs
+        for link in links:
+            print(link['href'])
+    else:
+        print(f"Error: {response.status_code}")
+
+
+# Example usage
+url_to_scrape = "https://sis.kalasalingam.ac.in/login"
+extract_urls(url_to_scrape)
